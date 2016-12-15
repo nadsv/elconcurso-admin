24914f8b0f8fdc5d93c22bfa7c1bbfac45917d12
Nadezhda Svezhintseva <svezhintzeva@ro46.fss.ru>
2016-12-15 15:20:47 +0300

    Add contest interface

---
+++ src/app/contest.ts
@@ -0,0 +1,8 @@
+export interface Contest {
+	id: number;
+	name: string;
+	active: boolean;
+	date: string;
+	description: string;
+	logo: string;
+}
--- src/app/form-contest/form-contest.component.ts
+++ src/app/form-contest/form-contest.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators  } from '@angular/forms';
+import { Contest } from '../contest';
 
 @Component({
-  selector: 'app-form-contest',
-  templateUrl: './form-contest.component.html',
-  styleUrls: ['./form-contest.component.scss']
+	selector: 'app-form-contest',
+	templateUrl: './form-contest.component.html',
+	styleUrls: ['./form-contest.component.scss']
 })
 export class FormContestComponent implements OnInit {
+	contest: FormGroup;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.contest = new FormGroup({
+      		name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      		active: new FormControl(''),
+      		date: new FormControl('', Validators.required),
+      		description: new FormControl(''),
+      		logo: new FormControl('')
+      	})
+	}
+
+	onSubmit() {
+		console.log(this.contest.value, this.contest.valid);
+	}
 
 }
